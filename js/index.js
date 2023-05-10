a63fb35fcce00aac167cb3e5a19c7ee1905826b4
oh-yeongji <dev.cappp@gmail.com>
2023-05-10 12:14:55 +0900

    feat:기본 js적용

---
+++ js/index.js
@@ -0,0 +1,185 @@
+// html , css ,js ,image ,font , vedio..
+// 사용되는 리소스가  모두 완료되고나서
+// js를 실행하여야 정상적인 처리가 가능하다.
+window.onload = function () {
+  //위로 이동하기
+  // .gotop을 js에 저장하자.
+  const goTop = document.querySelector(".gotop");
+  console.log(goTop);
+  console.log(typeof goTop);
+  // goTop의 클릭을 처리한다.
+  goTop.addEventListener("click", function () {
+    // 위로 슬라이딩 코드
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+      
+    });
+  });
+
+  //  <!-- Initialize Swiper -->
+
+  let promotionSwiper = new Swiper(".sw-promotion", {
+    slidesPerView: 1,
+    spaceBetween: 24,
+    speed: 1000,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+
+    navigation: {
+      nextEl: ".promotion .sw-next",
+      prevEl: ".promotion .sw-prev",
+    },
+    pagination: {
+      el: ".sw-promotion-pg",
+      // 클릭이 가능
+      clickable: true,
+    },
+
+    breakpoints: {
+      760: {
+        slidesPerView: 2,
+      },
+    },
+  });
+
+  // <!-- Shopping Swiper -->
+
+  let shoppingSwiper = new Swiper(".sw-shopping", {
+    slidesPerView: 5,
+    grid: {
+      rows: 2,
+      fill: "row",
+    },
+    spaceBetween: 10,
+    breakpoints: {
+      1024: {
+        spaceBetween: 32,
+        slidesPerView: 3,
+        // 화면당 3개씩 이동
+        slidesPerGroup: 3,
+
+        grid: {
+          rows: 1,
+        },
+      },
+      1280: {
+        spaceBetween: 26,
+        slidesPerView: 4,
+        // 화면당 4개씩 이동
+        slidesPerGroup: 4,
+        grid: {
+          rows: 1,
+        },
+      },
+    },
+  });
+
+  // <!-- tour swiper -->
+  let tourSwiper = new Swiper(".sw-tour", {
+    slidesPerView: 3,
+    grid: {
+      rows: 2,
+      fill: "row",
+    },
+    spaceBetween: 10,
+    breakpoints: {
+      1024: {
+        spaceBetween: 32,
+        slidesPerView: 2,
+        // 화면당 2개씩 슬라이드 이동
+        slidesPerGroup: 2,
+        grid: {
+          rows: 1,
+        },
+      },
+      1280: {
+        spaceBetween: 26,
+        slidesPerView: 3,
+        // 화면당 4개씩 슬라이드 이동
+        slidesPerGroup: 3,
+        grid: {
+          rows: 1,
+        },
+      },
+    },
+  });
+
+  // <!-- ticket swiper -->
+
+  let ticketSwiper = new Swiper(".sw-ticket", {
+    slidesPerView: "auto",
+    spaceBetween: 10,
+    breakpoints: {
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 32,
+      },
+      1280: {
+        slidesPerView: 4,
+        spaceBetween: 27,
+      },
+    },
+  });
+
+  // live swiper
+  let liveSwiper = new Swiper(".sw-live", {
+    slidesPerView: 4,
+    // 사진사이들 간격
+    spaceBetween: 10,
+    breakpoints: {
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 32,
+      },
+      1280: {
+        slidesPerView: 4,
+        spaceBetween: 27,
+      },
+    },
+  });
+
+  // books-swiper
+  let booksSwiper = new Swiper(".sw-books", {
+    slidesPerView: 3,
+    grid: {
+      rows: 4,
+      fill: "row",
+    },
+    spaceBetween: 19,
+    breakpoints: {
+      1024: {
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+        spaceBetween: 30,
+        grid: {
+          rows: 1,
+        },
+      },
+      1280: {
+        slidesPerView: 5,
+
+        slidesPerGroup: 5,
+        spaceBetween: 27,
+        grid: {
+          rows: 1,
+        },
+      },
+    },
+  });
+
+  // <!-- event-swiper -->
+
+  let eventSwiper = new Swiper(".sw-events", {
+    slidesPerView: 3,
+    spaceBetween: 27,
+    breakpoints: {
+      1280: {
+        slidesPerView: 4,
+      },
+    },
+  });
+};
