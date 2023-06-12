31e52f0e8f1712434eed29dac1cead7b2d8fc77d
oh-yeongji <dev.cappp@gmail.com>
2023-06-12 20:51:51 +0900

    feat:events section fetch작업 완료

--- js/events.js
+++ js/events.js
@@ -1,24 +1,28 @@
 window.addEventListener("load", function () {
   let data;
 
-  let xhr = new XMLHttpRequest();
+  // let xhr = new XMLHttpRequest();
 
-  xhr.onreadystatechange = function (e) {
-    let req = e.target;
-    if (req.readyState === XMLHttpRequest.DONE) {
-      data = JSON.parse(req.response);
-      makeEventSlide();
-    }
-  };
+  // xhr.onreadystatechange = function (e) {
+  //   let req = e.target;
+  //   if (req.readyState === XMLHttpRequest.DONE) {
+  //     data = JSON.parse(req.response);
+  //     makeEventSlide();
+  //   }
+  // };
 
-  xhr.open("GET", "data/events.json");
-  xhr.send();
+  // xhr.open("GET", "data/events.json");
+	fetch("data/events.json")
+	.then((res) => res.json())
+	.then((result)=>makeEventSlide(result))
+	.catch((err) => console.log(err))
+  // xhr.send();
 
   // let eventSwiper;
-  function makeEventSlide() {
+  function makeEventSlide(_data) {
     let swEventHtml = ``;
-    for (let i = 0; i < data.event_total; i++) {
-      let obj = data[`event_${i + 1}`];
+    for (let i = 0; i < _data.event_total; i++) {
+      let obj = _data[`event_${i + 1}`];
       let temp = `
 			<div class="swiper-slide">
 			<a href="${obj.link}" class="events-link">
